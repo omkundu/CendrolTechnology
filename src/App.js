2afcec78330d69a7a3be82415f5baa7159f5f13e
omkundu <omkundu156@gmail.com>
2023-08-06 10:31:27 +0530

    pushed-file-added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Categories from "./components/CategorySectionDiv";
+import Header from "./components/TopHeader";
 
 function App() {
+  const [categories, setcategories] = useState([]);
+  const [loding, setloding] = useState(false);
+
+  let fetchCategories = async () => {
+    setloding(true);
+    const fetchdata = await fetch(
+      "https://api.chucknorris.io/jokes/categories"
+    );
+    const jsondata = await fetchdata.json();
+    setloding(false);
+    setcategories(jsondata);
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loding ? (
+        <div className="flex flex-col items-center justify-center  mt-10">
+          {" "}
+          <div class="lds-facebook w-full  ">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        <div className="  h-fit flex flex-col items-center justify-center">
+          <Header />
+          <Categories categories={categories} />
+        </div>
+      )}
+    </>
   );
 }
 
